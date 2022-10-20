dc5c001f6cd3f58225b3256b414e29b60d3cf8f1
Owen Bonertz <owenbonertz@gmail.com>
2022-10-20 19:36:57 -0400

    Challenge 2 Loading

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(belgian chinese italian french japanese)}
+
+
 end
